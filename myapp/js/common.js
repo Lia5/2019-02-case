8eefdfb80d633358cab852f4fe9d09521b7e7ea5
Елена Портная <e.portnaya@largespace.ru>
2019-02-05 20:45:55 +0200

    footer + js

--- myapp/js/common.js
+++ myapp/js/common.js
@@ -7,7 +7,19 @@ $(function() {
 
 
 document.addEventListener('DOMContentLoaded', function(){
-//menu
+//go-top
+    if (document.querySelector('#go-top') != null) {
+        let toTop = document.querySelector('#go-top');
+    
+        window.onscroll = function () {
+            if (window.pageYOffset > 160) {
+                toTop.style.display = 'block';
+            } else {
+                toTop.style.display = "none";
+            }
+        }   
+    }
+    //menu
     var menu = document.querySelector('.menu-toggle');
     menu.addEventListener('click', function(){
         var nav = document.querySelector('.main-menu');
@@ -15,34 +27,32 @@ document.addEventListener('DOMContentLoaded', function(){
         var navGamb = document.querySelector('.menu-toggle');
         navGamb.classList.toggle('active');
     });
-//tabs
-	// store tabs variable
-	var myTabs = document.querySelectorAll("ul.header__tabs > li");
-    function myTabClicks(tabClickEvent) {
-		for (var i = 0; i < myTabs.length; i++) {
-			myTabs[i].classList.remove("active");
-		}
-		var clickedTab = tabClickEvent.currentTarget;
-		clickedTab.classList.add("active");
-		tabClickEvent.preventDefault();
-		var myContentPanes = document.querySelectorAll(".tab-pane");
-		for (i = 0; i < myContentPanes.length; i++) {
-			myContentPanes[i].classList.remove("active");
-		}
-        var anchorReference = tabClickEvent.target;
-        console.log(anchorReference);
-        var activePaneId = anchorReference.getAttribute("href");
-        console.log(activePaneId);
-        var activePane = document.querySelector(activePaneId);
-        console.log(activePaneId);
-		activePane.classList.add("active");
-    }
-    for (i = 0; i < myTabs.length; i++) {
-		myTabs[i].addEventListener("click", myTabClicks)
-	}
 
 
-
+//scrolling
+    var linkNav = document.querySelectorAll('[href^="#"]'), //выбираем все ссылки к якорю на странице
+    V = 0.25; // скорость, может иметь дробное значение через точку (чем меньше значение - тем больше скорость)
+    for (var i = 0; i < linkNav.length; i++) {
+    linkNav[i].addEventListener('click', function (e) { //по клику на ссылку
+        e.preventDefault(); //отменяем стандартное поведение
+        var w = window.pageYOffset, // производим прокрутка прокрутка
+            hash = this.href.replace(/[^#]*(.*)/, '$1'); // к id элемента, к которому нужно перейти
+        t = document.querySelector(hash).getBoundingClientRect().top, // отступ от окна браузера до id
+            start = null;
+        requestAnimationFrame(step); // подробнее про функцию анимации [developer.mozilla.org]
+        function step(time) {
+            if (start === null) start = time;
+            var progress = time - start,
+                r = (t < 0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
+            window.scrollTo(0, r);
+            if (r != w + t) {
+                requestAnimationFrame(step)
+            } else {
+                location.hash = hash // URL с хэшем
+            }
+        }
+    }, false);
+}
 
 
 });
